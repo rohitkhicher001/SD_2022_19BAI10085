06be2787584010c7c8a5c712fb69c5574cea9db8
rohit <rohitkhicher001@gmail.com>
2022-09-10 20:26:19 +0530

    Handled errors for invalid inputs and solved bug that moves were not relative

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
-void actionL(int &countA,int &countB,string position,vector<vector<string>> &v,unordered_map<string,pair<int,int>> &m)
+void actionL(bool turnA,int &countA,int &countB,string position,vector<vector<string>> &v,unordered_map<string,pair<int,int>> &m);
+void actionR(bool turnA,int &countA,int &countB,string position,vector<vector<string>> &v,unordered_map<string,pair<int,int>> &m);
+void actionF(bool turnA,int &countA,int &countB,string position,vector<vector<string>> &v,unordered_map<string,pair<int,int>> &m);
+void actionB(bool turnA,int &countA,int &countB,string position,vector<vector<string>> &v,unordered_map<string,pair<int,int>> &m);
+
+
+void actionL(bool turnA,int &countA,int &countB,string position,vector<vector<string>> &v,unordered_map<string,pair<int,int>> &m)
 {
    string temp = position;
    int left = m[position].first;
    int right = m[position].second;
+   if(turnA)
+   {
    if(v[left][right-1]!= "#")
    {
 
@@ -26,59 +34,160 @@ void actionL(int &countA,int &countB,string position,vector<vector<string>> &v,u
    v[left][right-1] = v[left][right];
    v[left][right]="#";
    m[temp] = {left,right-1};
+   }
+   else{
+     actionR(turnA,countA,countB,position,v,m);
+   }
 }
-void actionR(string position,vector<vector<string>> &v,unordered_map<string,pair<int,int>> &m)
+void actionR(bool turnA,int &countA,int &countB,string position,vector<vector<string>> &v,unordered_map<string,pair<int,int>> &m)
 {
 string temp = position;
    int left = m[position].first;
    int right = m[position].second;
+   if(turnA)
+   {
+
+   if(v[left][right+1]!= "#")
+   {
+
+       char first_character = v[left][right+1][0];
+       if(first_character == 'A' && v[left][right][0] == 'B')
+       {
+           countA--;
+       }
+       else if(first_character == 'B' && v[left][right][0] == 'A')
+       {
+
+           countB--;
+       }
+       else{
+        cout<<"You cannot kill your own troops"<<endl;
+        return;
+       }
+    }
    v[left][right+1] = v[left][right];
    v[left][right]="#";
    m[temp] = {left,right+1};
-}void actionF(string position,vector<vector<string>> &v,unordered_map<string,pair<int,int>> &m)
+   }
+   else
+   {
+      actionL(turnA,countA,countB,position,v,m);
+   }
+}void actionF(bool turnA,int &countA,int &countB,string position,vector<vector<string>> &v,unordered_map<string,pair<int,int>> &m)
 {
     string temp = position;
    int left = m[position].first;
    int right = m[position].second;
+   if(turnA)
+   {
+   if(v[left-1][right]!= "#")
+   {
+
+       char first_character = v[left-1][right][0];
+       if(first_character == 'A' && v[left][right][0] == 'B')
+       {
+           countA--;
+       }
+       else if(first_character == 'B' && v[left][right][0] == 'A')
+       {
+
+           countB--;
+       }
+       else{
+        cout<<"You cannot kill your own troops"<<endl;
+        return;
+       }
+    }
    v[left-1][right] = v[left][right];
    v[left][right]="#";
    m[temp] = {left-1,right};
+   }
+   else
+   {
+       actionB(turnA,countA,countB,position,v,m);
+   }
 
-}void actionB(string position,vector<vector<string>> &v,unordered_map<string,pair<int,int>> &m)
+}void actionB(bool turnA,int &countA,int &countB,string position,vector<vector<string>> &v,unordered_map<string,pair<int,int>> &m)
 {
-    cout<<"first success";
+
 string temp = position;
    int left = m[position].first;
    int right = m[position].second;
+   if(turnA)
+   {
+   if(v[left+1][right]!= "#")
+   {
+
+       char first_character = v[left+1][right][0];
+       if(first_character == 'A' && v[left][right][0] == 'B')
+       {
+           countA--;
+       }
+       else if(first_character == 'B' && v[left][right][0] == 'A')
+       {
+
+           countB--;
+       }
+       else{
+        cout<<"You cannot kill your own troops"<<endl;
+        return;
+       }
+    }
    v[left+1][right] = v[left][right];
    v[left][right]="#";
    m[temp] = {left+1,right};
-   cout<<"success2";
+   }
+   else
+   {
+
+       actionF(turnA,countA,countB,position,v,m);
+   }
+
 }
-void actioninput(int &countA,int &countB,vector<vector<string>> &v,unordered_map<string,pair<int,int>> &m)
+void actioninput(bool turnA,int &countA,int &countB,vector<vector<string>> &v,unordered_map<string,pair<int,int>> &m)
 {
     cout<<"Choose player and action (Example: A-p2 L)"<<endl;
     string position;
     cin>>position;
+    if(position[0]!= 'A' || position[0] != 'B' || position[1]!= '-' || position[2]!='p' || position[3]>'5' || position[3]<'1' )
+    {
+        cout<<"Invalid position"<<endl;
+        actioninput(turnA,countA,countB,v,m);
+    }
     string action;
     cin>>action;
+    if(action!="L" || action!= "R" || action!= "F" || action != "B")
+    {
+        actioninput(turnA,countA,countB,v,m);
+    }
     if(action == "L")
     {
-        actionL(countA,countB,position,v,m);
+        actionL(turnA,countA,countB,position,v,m);
     }
     else  if(action == "R")
     {
-        actionR(position,v,m);
+        actionR(turnA,countA,countB,position,v,m);
     }
     else  if(action == "F")
     {
-        actionF(position,v,m);
+        actionF(turnA,countA,countB,position,v,m);
     }
     else  if(action == "B")
     {
-        cout<<"success0";
-        actionB(position,v,m);
-        cout<<"success1";
+
+        actionB(turnA,countA,countB,position,v,m);
+
+    }
+
+}
+void input_vector(vector<int> &v)
+{
+    for(int i=0;i<5;i++)
+    {
+
+        int choice;
+        cin>>choice;
+        v.push_back(choice-1);
     }
 
 }
@@ -92,13 +201,18 @@ vector<string> input()
         cout<<i<<" ";
     }
     cout<<endl;
-    vector<int> choices;
+    vector<int> choices,tempchoices;
+    input_vector(choices);
     for(int i=0;i<5;i++)
     {
-        int choice;
-        cin>>choice;
-        choices.push_back(choice-1);
+        if(choices[i]>4 || choices[i]<0)
+        {
+            cout<<"Please enter a number between 1 and 5"<<endl;
+            choices = tempchoices;
+            input_vector(choices);
+        }
     }
+
 vector<string> A_characters;
     cout<<"Your characters are : ";
     for(int i=0;i<5;i++)
@@ -152,9 +266,11 @@ int main() {
         }
     int countA =5;
     int countB= 5;
+    bool turnA = true;
     while(countA && countB)
     {
-        actioninput(countA,countB,v,m);
+        actioninput(turnA,countA,countB,v,m);
+        turnA = !turnA;
         for(auto i: v)
         {
             for(auto j: i)
@@ -166,6 +282,16 @@ int main() {
             cout<<endl;
         }
         cout<<countA<<" "<<countB<<endl;
+    }
+    if(countA == 0)
+    {
+
+        cout<<"Player B wins"<<endl;
+
+    }
+    else if(countB == 0)
+    {
+        cout<<"Player A wins"<<endl;
     }
 
     return 0;
