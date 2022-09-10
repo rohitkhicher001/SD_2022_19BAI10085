bc50e1a7b73c8881e474a9dc7bc5342a3c27ed59
rohit <rohitkhicher001@gmail.com>
2022-09-10 19:14:12 +0530

    implemented functions to move characters

--- main.cpp
+++ main.cpp
@@ -1,6 +1,65 @@
 #include <bits/stdc++.h>
 using namespace std;
+void actionL(string position,vector<vector<string>> &v,unordered_map<string,pair<int,int>> &m)
+{
+   string temp = position;
+   int left = m[position].first;
+   int right = m[position].second;
+   v[left][right-1] = v[left][right];
+   v[left][right]="";
+   m[temp] = {left,right-1};
+}
+void actionR(string position,vector<vector<string>> &v,unordered_map<string,pair<int,int>> &m)
+{
+string temp = position;
+   int left = m[position].first;
+   int right = m[position].second;
+   v[left][right+1] = v[left][right];
+   v[left][right]="";
+   m[temp] = {left,right+1};
+}void actionF(string position,vector<vector<string>> &v,unordered_map<string,pair<int,int>> &m)
+{
+    string temp = position;
+   int left = m[position].first;
+   int right = m[position].second;
+   v[left+1][right] = v[left][right];
+   v[left][right]="";
+   m[temp] = {left+1,right};
 
+}void actionB(string position,vector<vector<string>> &v,unordered_map<string,pair<int,int>> &m)
+{
+string temp = position;
+   int left = m[position].first;
+   int right = m[position].second;
+   v[left][right-1] = v[left][right];
+   v[left][right]="";
+   m[temp] = {left,right-1};
+}
+void actioninput(vector<vector<string>> &v,unordered_map<string,pair<int,int>> &m)
+{
+    cout<<"Choose player and action (Example: A-p2 L)"<<endl;
+    string position;
+    cin>>position;
+    string action;
+    cin>>action;
+    if(action == "L")
+    {
+        actionL(position,v,m);
+    }
+    else  if(action == "R")
+    {
+        actionR(position,v,m);
+    }
+    else  if(action == "F")
+    {
+        actionF(position,v,m);
+    }
+    else  if(action == "B")
+    {
+        actionB(position,v,m);
+    }
+
+}
 vector<string> input()
 {
 
@@ -43,19 +102,48 @@ int main() {
     vector<string> B_characters = input();
     for(int i=0;i<5;i++)
     {
-        v[0][i] = A_characters[i];
+        v[0][i] = "A-"+A_characters[i];
     }
      for(int i=0;i<5;i++)
     {
-        v[4][i] = B_characters[i];
+        v[4][i] = "B-"+B_characters[i];
     }
-      for(auto i: v)
+     unordered_map<string,pair<int,int>> m;
+    for(int i=0;i<5;i++)
     {
-        for(auto j: i)
+
+        for(int j=0;j<5;j++)
         {
-            cout<<j<<" ";
+            m[v[i][j]] = {i,j};
         }
-        cout<<endl;
     }
+
+ for(auto i: v)
+        {
+            for(auto j: i)
+            {
+
+                cout<<j<<" ";
+
+            }
+            cout<<endl;
+        }
+    int countA =5;
+    int countB= 5;
+    while(true)
+    {
+        actioninput(v,m);
+        for(auto i: v)
+        {
+            for(auto j: i)
+            {
+
+                cout<<j<<" ";
+
+            }
+            cout<<endl;
+        }
+    }
+
     return 0;
 }
